2f9e1e63b1893964c192a364caf05335ac542715
NathanLabbe <nathanlab1997@gmail.com>
2020-10-22 17:16:51 +0200

    tt

--- jpeg2000/j2k.sh
+++ jpeg2000/j2k.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #pngquant --quality=1 ILSVRC2012_val_00000023.JPEG
 
-for file in ../png/originalImg/*; 
+for file in ../png/originalImg/* 
 do 
   file2=$(basename -- "$file")
   file3=${file2::-4}
-  opj_compress -i $file -t 500,500 -o ./originalImg/$file2.j2k
+  opj_compress -i $file -t 500,500 -o ./originalImg/$file3.j2k
 done
