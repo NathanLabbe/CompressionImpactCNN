ff61add2da26d00aa3ac615bbc45c4b2f8f124fb
NathanLabbe <nathanlab1997@gmail.com>
2020-10-27 17:00:31 +0100

    jpeg + R

--- plots.R
+++ plots.R
@@ -6,48 +6,57 @@ library('png')
 library('ggplot2')
 
 rm(sizes)
-files <- list.files(path="/home/rob/Documents/CompressionImpactCNN/jpeg/outputImg/", pattern="*.jpeg", full.names=TRUE, recursive=FALSE)
+
+#AJOUT DE JPEG
+files <- list.files(path="./jpeg/outputImg/", pattern="*.jpeg", full.names=TRUE, recursive=FALSE)
 sizes <- data.frame("name" = character(), "format"= character(), "size"= double(), "pixels"= double(), "compressionRate"=double())
-#sizes <- rbind(sizes, file.size("/home/rob/Documents/CompressionImpactCNN/jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
+#sizes <- rbind(sizes, file.size("./jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
 for (file in files){
   
-  pathInput="/home/rob/Documents/CompressionImpactCNN/inputImg/"
+  pathInput="./inputImg/"
   pathInput <- paste(pathInput, substr(basename(file.path(file)), 1, 23))
   pathInput <- paste(pathInput, ".JPEG")
   pathInput <- gsub(" ", "", pathInput)
   #file.size(pathInput)
   compressionRate = file.size(pathInput)/file.size(file)
   
-  img <- readJPEG(file.path(file))
-  pixels = ncol(img) * nrow(img)
-  tmp <- data.frame("name" = basename(file.path(file)),"format"="jpeg", "size" = file.size(file), "pixels"=pixels, "compressionRate"= compressionRate)
+  #img <- readJPEG(file.path(file))
+  #pixels = ncol(img) * nrow(img)
+  tmp <- data.frame("name" = basename(file.path(file)),"format"="jpeg", "size" = file.size(file), "pixels"=0, "compressionRate"= compressionRate)
   
   sizes <- rbind(sizes, tmp)
 }
 
-files <- list.files(path="/home/rob/Documents/CompressionImpactCNN/png/outputImg/", pattern="*.png", full.names=TRUE, recursive=FALSE)
-#sizes <- rbind(sizes, file.size("/home/rob/Documents/CompressionImpactCNN/jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
+
+#AJOUT DE PNG
+files <- list.files(path="./png/outputImg/", pattern="*.png", full.names=TRUE, recursive=FALSE)
+#sizes <- rbind(sizes, file.size("./jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
 for (file in files){
   
-  pathInput="/home/rob/Documents/CompressionImpactCNN/png/originalImg/"
+  pathInput="./inputImg/"
   pathInput <- paste(pathInput, substr(basename(file.path(file)), 1, 23))
-  pathInput <- paste(pathInput, ".png")
+  pathInput <- paste(pathInput, ".JPEG")
   pathInput <- gsub(" ", "", pathInput)
   #file.size(pathInput)
+  print(pathInput)
+  #print(file)
   compressionRate = file.size(pathInput)/file.size(file)
   
-  img <- readPNG(file.path(file))
-  pixels = ncol(img) * nrow(img)
-  tmp <- data.frame("name" = basename(file.path(file)),"format"="png", "size" = file.size(file), "pixels"=pixels, "compressionRate"= compressionRate)
+  #img <- readPNG(file.path(file))
+  #pixels = ncol(img) * nrow(img)
+  tmp <- data.frame("name" = basename(file.path(file)),"format"="png", "size" = file.size(file), "pixels"=0, "compressionRate"= compressionRate)
   
   sizes <- rbind(sizes, tmp)
 }
 
-files <- list.files(path="/home/rob/Documents/CompressionImpactCNN/bpg/outputImg/", pattern="*.bpg", full.names=TRUE, recursive=FALSE)
-#sizes <- rbind(sizes, file.size("/home/rob/Documents/CompressionImpactCNN/jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
+
+
+#AJOUT DE BPG
+files <- list.files(path="./bpg/outputImg/", pattern="*.bpg", full.names=TRUE, recursive=FALSE)
+#sizes <- rbind(sizes, file.size("./jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
 for (file in files){
   
-  pathInput="/home/rob/Documents/CompressionImpactCNN/inputImg/"
+  pathInput="./inputImg/"
   pathInput <- paste(pathInput, substr(basename(file.path(file)), 1, 23))
   pathInput <- paste(pathInput, ".JPEG")
   pathInput <- gsub(" ", "", pathInput)
@@ -60,11 +69,12 @@ for (file in files){
   sizes <- rbind(sizes, tmp)
 }
 
-files <- list.files(path="/home/rob/Documents/CompressionImpactCNN/jpeg2000/outputImg/", pattern="*.j2k", full.names=TRUE, recursive=FALSE)
-#sizes <- rbind(sizes, file.size("/home/rob/Documents/CompressionImpactCNN/jpeg/outputImg/ILSVRC2012_val_00000023.2_1.jpeg"))
+
+#AJOUT DE J2K
+files <- list.files(path="./jpeg2000/outputImg/", pattern="*.j2k", full.names=TRUE, recursive=FALSE)
 for (file in files){
   
-  pathInput="/home/rob/Documents/CompressionImpactCNN/inputImg/"
+  pathInput="./inputImg/"
   pathInput <- paste(pathInput, substr(basename(file.path(file)), 1, 23))
   pathInput <- paste(pathInput, ".JPEG")
   pathInput <- gsub(" ", "", pathInput)
@@ -107,6 +117,74 @@ for (name in sizes$name){
 }
 bpg_4_40 <- mean(bpg$compression)
 
+
+
+#TABLEAU PNG
+#1_30
+png <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "1_30.png")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    png <- rbind(png, tmp)
+  }
+}
+png_1_30 <- mean(png$compression)
+medpng_1_30 <- median(png$compression)
+sdpng_1_30 <- sd(png$compression)
+#2_40
+png <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "2_40.png")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    png <- rbind(png, tmp)
+  }
+}
+png_2_40 <- mean(png$compression)
+medpng_2_40 <- median(png$compression)
+sdpng_2_40 <- sd(png$compression)
+#4_50
+png <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "4_50.png")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    png <- rbind(png, tmp)
+  }
+}
+png_4_50 <- mean(png$compression)
+medpng_4_50 <- median(png$compression)
+sdpng_4_50 <- sd(png$compression)
+#8_50
+png <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "8_50.png")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    png <- rbind(png, tmp)
+  }
+}
+png_8_50 <- mean(png$compression)
+medpng_8_50 <- median(png$compression)
+sdpng_8_50 <- sd(png$compression)
+
+
+
+#TABLEAU JPEG 
+#1_30
+jpeg <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "1_30.jpeg")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    jpeg <- rbind(jpeg, tmp)
+  }
+}
+jpeg_1_30 <- mean(jpeg$compression)
+medjpeg_1_30 <- median(jpeg$compression)
+sd_1_30 <- sd(jpeg$compression)
+#2_40
 jpeg <- data.frame("name" = character(), "compression" = double())
 for (name in sizes$name){
   if(endsWith(name, "2_40.jpeg")){
@@ -118,15 +196,70 @@ for (name in sizes$name){
 jpeg_2_40 <- mean(jpeg$compression)
 medjpeg_2_40 <- median(jpeg$compression)
 sd_2_40 <- sd(jpeg$compression)
+#4_50
+jpeg <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "4_50.jpeg")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    jpeg <- rbind(jpeg, tmp)
+  }
+}
+jpeg_4_50 <- mean(jpeg$compression)
+medjpeg_4_50 <- median(jpeg$compression)
+sd_4_50 <- sd(jpeg$compression)
+#8_50
+jpeg <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "8_50.jpeg")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    jpeg <- rbind(jpeg, tmp)
+  }
+}
+jpeg_8_50 <- mean(jpeg$compression)
+medjpeg_8_50 <- median(jpeg$compression)
+sd_8_50 <- sd(jpeg$compression)
 
+
+#TABLEAU J2K
+#1_200,40
 j2k <- data.frame("name" = character(), "compression" = double())
 for (name in sizes$name){
-  if(endsWith(name, "2_200,40,20.j2k")){
+  if(endsWith(name, "1_200,40.j2k")){
     cr <- sizes$compressionRate[sizes$name == name]
     tmp <- data.frame("name" = name, "compression" = cr)
     j2k <- rbind(j2k, tmp)
   }
 }
-j2k_2_200_40_20 <- mean(j2k$compression)
+j2k_1_200_40 <- mean(j2k$compression)
+medj2k_1_200_40<- median(j2k$compression)
+sdj2k_1_200_40 <- sd(j2k$compression)
+#1_50
+j2k <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "1_50.j2k")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    j2k <- rbind(j2k, tmp)
+  }
+}
+j2k_1_50 <- mean(j2k$compression)
+medj2k_1_50<- median(j2k$compression)
+sdj2k_1_50 <- sd(j2k$compression)
+#2_100,20,10
+j2k <- data.frame("name" = character(), "compression" = double())
+for (name in sizes$name){
+  if(endsWith(name, "2_100,20,10.j2k")){
+    cr <- sizes$compressionRate[sizes$name == name]
+    tmp <- data.frame("name" = name, "compression" = cr)
+    j2k <- rbind(j2k, tmp)
+  }
+}
+j2k_2_100_20_10 <- mean(j2k$compression)
+medj2k_2_100_20_10<- median(j2k$compression)
+sdj2k_2_100_20_10 <- sd(j2k$compression)
+
+
 
 
